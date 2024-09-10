7d8cd0f61ccbf522f5b9649eed85227cbe8ada1d
sykez <hello@sykez.com>
2024-09-11 01:15:55 +0800

    V2

--- app/Http/Controllers/V2/MyController.php
+++ app/Http/Controllers/V2/MyController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\V2;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class MyController extends Controller
@@ -12,19 +11,5 @@ class MyController extends Controller
         return response()->json([
             'message' => 'Hello World V2'
         ], 200);
-    }
-
-    public function store(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|email',
-        ]);
-
-        // Save to database
-
-        return response()->json([
-            'message' => "User {$request->name} created!"
-        ], 201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,8 @@ Route::get('/test', 'App\Http\Controllers\MyController@index');
 Route::post('/test', 'App\Http\Controllers\MyController@store');
 Route::post('/test/{id}', 'App\Http\Controllers\MyController@update');
 Route::delete('/test/{id}', 'App\Http\Controllers\MyController@destroy');
+
+Route::group(['prefix' => 'v2', 'namespace' => 'App\Http\Controllers\V2'], function () {
+    Route::get('/test', 'MyController@index');
+    Route::post('/test', 'MyController@store');
+});
