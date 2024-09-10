2ef52cbc5c6ece7235b82bbcdf92e334655080e6
sykez <hello@sykez.com>
2024-09-11 01:12:34 +0800

    fix

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/test', 'MyController@index');
-Route::post('/test', 'MyController@store');
-Route::put('/test/{id}', 'MyController@update');
-Route::delete('/test/{id}', 'MyController@destroy');
+Route::get('/test', 'App\Http\Controllers\MyController@index');
+Route::post('/test', 'App\Http\Controllers\MyController@store');
+Route::post('/test/{id}', 'App\Http\Controllers\MyController@update');
+Route::delete('/test/{id}', 'App\Http\Controllers\MyController@destroy');
