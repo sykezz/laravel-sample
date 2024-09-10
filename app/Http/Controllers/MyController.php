547aec58ceb573d478187386baa86ade07e05712
sykez <hello@sykez.com>
2024-09-11 00:51:33 +0800

    Reset

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -6,10 +6,5 @@ use Illuminate\Http\Request;
 
 class MyController extends Controller
 {
-    public function index()
-    {
-        return response()->json([
-            'message' => 'Hello World'
-        ], 200);
-    }
+    //
 }
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,8 @@
 <?php
 
-use App\Http\Controllers\MyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::get('/test', 'MyController@index');
